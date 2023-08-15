0250cddc45d494717a9aa23276ba0f25b2c18cc0
blezzydcruz <mbannedelacruz@hotmail.com>
2023-08-15 16:19:38 +0100

    completed customer

--- src/main/java/com/bnta/bnta_backend_api_project/services/CustomerService.java
+++ src/main/java/com/bnta/bnta_backend_api_project/services/CustomerService.java
@@ -2,6 +2,7 @@ package com.bnta.bnta_backend_api_project.services;
 
 import com.bnta.bnta_backend_api_project.controllers.CustomerController;
 import com.bnta.bnta_backend_api_project.models.Customer;
+import com.bnta.bnta_backend_api_project.models.CustomerDTO;
 import com.bnta.bnta_backend_api_project.repositories.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,5 +21,23 @@ public class CustomerService {
 
     public Customer findCustomer(Long id) {
         return this.customerRepository.findById(id).get();
+    }
+
+    public Customer addCustomer(CustomerDTO customerDTO) {
+        Customer customer1 = new Customer(customerDTO.getName(), customerDTO.getEmail(), customerDTO.getCardNumber());
+        return this.customerRepository.save(customer1);
+    }
+
+    public Customer updateCustomer(CustomerDTO customerDTO, Long id) {
+        Customer customerToUpdate = customerRepository.findById(id).get();
+        customerToUpdate.setName(customerDTO.getName());
+        customerToUpdate.setEmail(customerDTO.getEmail());
+        customerToUpdate.setCardNumber(customerDTO.getCardNumber());
+        customerRepository.save(customerToUpdate);
+        return customerToUpdate;
+    }
+
+    public void deleteCustomer(Long id) {
+        customerRepository.deleteById(id);
     }
 }
